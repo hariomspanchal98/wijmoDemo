ab1fa1c70744c0b2cd11eb0703b68dc2714aa06b
HariOm <hariomspanchal98@gmail.com>
2023-04-27 10:15:13 +0530

    wijmoIntegration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { CollectionView } from '@grapecity/wijmo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'wijmoDemo';
+  title = 'WijmoDemo';
+  data = this.getData();
+  getData() {
+    var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(','),
+      data = [];
+    for (var i = 0; i < countries.length; i++) {
+      data.push({
+        country: countries[i],
+        sales: Math.random() * 10000,
+        expenses: Math.random() * 5000,
+        downloads: Math.round(Math.random() * 20000),
+      });
+    }
+    return new CollectionView(data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { WjChartModule } from '@grapecity/wijmo.angular2.chart';
+import { WjGridFilterModule } from '@grapecity/wijmo.angular2.grid.filter';
+import { WjGridSearchModule } from '@grapecity/wijmo.angular2.grid.search';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjGridModule,
+    WjChartModule,
+    WjGridFilterModule,
+    WjGridSearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
